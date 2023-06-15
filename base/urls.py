d09447811ff275428717b0aa554d6a851f52d70f
BouzieneNacef <bouzaienne1995@gmail.com>
2023-06-15 02:38:50 +0200

    Product and Command API_View

--- base/urls.py
+++ base/urls.py
@@ -7,9 +7,9 @@ from .viewSets import *
 # get instance of the router defined in rest_framework
 router = routers.DefaultRouter()
 # add Product and client urls( get, post, put, delete) 
-router.register(r'product', ProductViewSet, basename='product') 
+#router.register(r'product', ProductViewSet, basename='product') 
 router.register(r'client', ClientViewSet, basename='clent')
-router.register(r'command',CommandViewSet,basename='command')
+#router.register(r'command',CommandViewSet,basename='command')
 router.register(r'details',CommandDetailsViewSet,basename='command_details')
 router.register(r'category',CategoryViewSet,basename='product_category')
 router.register(r'pannier',PannierViewSet,basename='pannier')
@@ -17,7 +17,7 @@ router.register(r'delivery',DeliveryInformationViewSet,basename='delivery')
 
  
 from rest_framework_simplejwt.views import (
-    TokenRefreshView,
+    TokenRefreshView,                                                                           
 )
 
 urlpatterns = [
@@ -26,7 +26,11 @@ path('base/', views.getRoutes),
 path('auth/token/', MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
 path('auth/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 path(r'product/add', views.add_product),
+path(r'product/all', views.get_all_product),
 path(r'product/<int:id>', views.retreive_update_or_delete_product),
+path(r'command/add', views.add_command),
+path(r'command/all', views.get_all_command),
+path(r'command/<int:id>', views.retreive_update_or_delete_command),
 
 #path('product/', views.product_crud, name="product"),
 
--- base/views.py
+++ base/views.py
@@ -53,6 +53,14 @@ def add_product(request):
         return Response(product.errors,status=status.HTTP_400_BAD_REQUEST)
    
     return JsonResponse({"message":"The method is not allowed"},status=status.HTTP_405_METHOD_NOT_ALLOWED)
+
+# get all of the product
+@api_view(['GET'])
+def get_all_product(request):
+    product = Product.objects.all()
+    serializer = ProductSerializer(product, many=True)
+    return Response(serializer.data, status=status.HTTP_200_OK)
+
 # retreive, update and delete product            
 @api_view(['GET','PUT','DELETE'])
 def retreive_update_or_delete_product(request,id):
@@ -79,4 +87,46 @@ def retreive_update_or_delete_product(request,id):
             return Response(status=status.HTTP_404_NOT_FOUND)
 
 
+# command crud:
+
+@api_view(['POST'])
+def add_command(request):
+    if request.method=='POST':
+        command=CommandSerializer(data=request.data) #get the comand object from the request after deserialization
+        if command.is_valid(): #check if the command object is valid (all required fields are filled and fields data types and format are correct)
+            command.save()
+            return Response(status=status.HTTP_201_CREATED)
+        return Response(command.errors,status=status.HTTP_400_BAD_REQUEST)
+   
+    return JsonResponse({"message":"The method is not allowed"},status=status.HTTP_405_METHOD_NOT_ALLOWED)
+# retreive, update and delete command            
+@api_view(['GET','PUT','DELETE'])
+def retreive_update_or_delete_command(request,id):
+        try:
+            command=Command.objects.get(pk=id)
+            if request.method=='GET':
+    
+                serializer=CommandSerializer(command)
+                return Response(serializer.data,status=status.HTTP_200_OK)
+       
+            elif request.method=='PUT':
+              serialzer=CommandSerializer(command,data=request.data)#get group informations from the request and update the instance of group geted by nid from the DB.
+              if serialzer.is_valid():
+                serialzer.save()
+                return Response(status=status.HTTP_202_ACCEPTED)
+              return Response(serialzer.errors,status=status.HTTP_400_BAD_REQUEST)
+            elif request.method=='DELETE':
+                command.delete()
+                return Response(status=status.HTTP_204_NO_CONTENT) # to say that the deleted group no longer exists
+                #or
+                #return JsonResponse({"message":"The group was successuflly deleted"},status=status.HTTP_202_ACCEPTED)
+            return JsonResponse({"message":"The method is not allowed"},status=status.HTTP_405_METHOD_NOT_ALLOWED)
+        except Command.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+        
+@api_view(['GET'])
+def get_all_command(request):
+    command = Command.objects.all()
+    serializer = CommandSerializer(command, many=True)
+    return Response(serializer.data, status=status.HTTP_200_OK)
 
